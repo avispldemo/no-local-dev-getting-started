f743c2ea40d491e139878342589b74b2fef12eb3
avispldemo <shane.baumgartner@avispl.com>
2016-12-01 16:34:39 -0600

    Update app.rb

--- app.rb
+++ app.rb
@@ -4,14 +4,23 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './environments'
 
-class Account < ActiveRecord::Base
-  self.table_name = 'salesforce.account'
+#class Account < ActiveRecord::Base
+#  self.table_name = 'salesforce.account'
+#end
+
+class Contact < ActiveRecord::Base
+  self.table_name = 'salesforce.contact'
 end
 
-get "/accounts" do
-  @accounts = Account.all
+get "/contacts" do
+  @contacts = Contact.all
   erb :index
 end
+
+#get "/accounts" do
+#  @accounts = Account.all
+#  erb :index
+#end
 
 get "/" do
   erb :home
